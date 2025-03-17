a2d9fa008b156623726a0c1fe4f50cf397f6d8d1
hardeep8587 <hardeep8587@gmail.com>
2025-03-17 16:47:40 -0700

    added strip function

--- quiz.py
+++ quiz.py
@@ -1,25 +1,25 @@
 print("Welcome to computer quiz!")
 playing = input("Do you want to play? ")
 score = 0
-if playing.lower() != 'yes':
+if playing.strip().lower() != 'yes':
     quit()
 print('Lets play the game! :)')
 answer = input('What does CPU stand for?: ')
-if answer.lower() == 'central processing unit':
+if answer.strip().lower() == 'central processing unit':
     print('Correct!')
     score += 1
 else: 
     print('Incorrect!')
 
 answer = input('What does RAM stand for?: ')
-if answer.lower() == 'random access memory':
+if answer.strip().lower() == 'random access memory':
     print('Correct!')
     score += 1
 
 else: 
     print('Incorrect!')
 answer = input('What does ROM stand for?: ')
-if answer.lower() == 'read only memory':
+if answer.strip().lower() == 'read only memory':
     print('Correct!')
     score += 1
 else: 
