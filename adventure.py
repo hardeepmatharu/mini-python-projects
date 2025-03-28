5fa34373fcdb2b8c40d0235391167f489b83338b
hardeep8587 <hardeep8587@gmail.com>
2025-03-27 21:20:00 -0700

    final tweeks

--- adventure.py
+++ adventure.py
@@ -9,11 +9,24 @@ if answer == 'left':
    if answer == 'walk':
      print('You walked many miles across the river and you ran out of water and you lost the game.')
    elif answer == 'swim':
-     print('You swam across the and you were eaten by an alligator. You lost.')
+     print('You swam across the river and you were eaten by an alligator. You lost.')
    else:
      print('Not a valid option. You lose.')
 
 elif answer == 'right':
-  pass
+    answer = input('You come to a bridge, it is wobbly. You want to cross it or go back(cross/back)? ')
+    if answer == 'cross':
+      print('You crossed the bridge and you meet a stranger. You talk to stranger. yes/no? ')
+      if answer == 'yes':
+        print('You talked to a stranger and he gave you gold and you WIN!')
+      elif answer == 'no':
+        print('You ignored the stranger and he got offended and you lose.')
+      else:
+        print('Not a valid option. You lose.')
+    elif answer == 'back':
+      print('You go back. You lost.')
+    else:
+      print('Not a valid option. You lose.')
 else:
   print('Not a valid option. You lose.')
+print('Thank you',user_name, 'for playing this game!')
