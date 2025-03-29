a741b0a2b3a13c0ee6b4aece64dd116ebf0cc278
hardeep8587 <hardeep8587@gmail.com>
2025-03-28 22:21:12 -0700

    updated password added cryptography

--- password.py
+++ password.py
@@ -1,4 +1,21 @@
-master_pwd = input('What is the master password? ')
+from cryptography.fernet import Fernet
+
+#write key into a key file
+'''def write_key():
+    key = Fernet.generate_key()
+    with open('key.key','wb') as key_file:
+        key_file.write(key)'''
+
+
+#load key to decrypt
+def load_key():
+    f = open('key.key', 'rb')
+    key = f.read()
+    f.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
 
 #function to add password
 def add_pwd():
@@ -6,12 +23,18 @@ def add_pwd():
     pwd = input('Enter the password: ')
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + '|' + pwd + '\n')
+        f.write(name + '|' + fer.encrypt(pwd.encode()).decode() + '\n')
+
+
 #function to view password
 def view_pwd():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
-            print(line.rstrip())
+            data = line.rstrip()
+            user,passw = data.split('|')
+            print('User:', user, '| Passowrd:',fer.decrypt(passw.encode()).decode())
+
+
 
 while True:
     mode = input('Would you like to create a new password or view the existing one? (view/add) or press q to quit. ')
