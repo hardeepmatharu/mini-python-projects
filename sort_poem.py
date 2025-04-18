3a2f05e00a5d7c3ef92556285ec79de58511a3ab
hardeep8587 <hardeep8587@gmail.com>
2025-04-17 20:26:44 -0700

    program to get python version using platform module

--- sort_poem.py
+++ sort_poem.py
@@ -9,6 +9,5 @@ Twinkle, twinkle, little star,
 Twinkle, twinkle, little star, 
 	How I wonder what you are'''
 
-
 x = "Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high,\n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are"
 print(x)
